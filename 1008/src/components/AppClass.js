5f98d6d3a12dce52bd4e57a51023b9dba9154522
Maureen <chiamaureenla@gmail.com>
2020-10-12 15:56:39 +0800

    當子女元件需要調整prop的時候

--- 1008/src/components/AppClass.js
+++ 1008/src/components/AppClass.js
@@ -1,5 +1,6 @@
 //導入react函式庫
 import React from 'react'
+import PropTypes from 'prop-types'
 
 //繼承類別
 class AppClass extends React.Component {
@@ -15,11 +16,13 @@ class AppClass extends React.Component {
 
   // render的回傳值即為最後呈現在網頁上的元素
   render() {
-    console.log(this.props)
+    //console.log(this.props)
     return (
       <>
+        {/* 呈現狀態值要使用this.state.total */}
         <h1>{this.props.title}</h1>
         <h1>{this.state.total}</h1>
+        {/* 設定/更動狀態要呼叫this.setState方法，給定之後狀態要改變的物件樣子 */}
         <button onClick={() => this.setState({ total: this.state.total + 1 })}>
           +1
         </button>
@@ -29,6 +32,16 @@ class AppClass extends React.Component {
       </>
     )
   }
+}
+
+AppClass.propTypes = {
+  initValue: PropTypes.number,
+  title: PropTypes.string,
+}
+
+AppClass.defaultProps = {
+  initValue: 0,
+  title: '沒標題',
 }
 
 //輸出函式
--- 1008/src/components/AppFunction.js
+++ 1008/src/components/AppFunction.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 
 //自訂元件 開頭一定要大寫 ex:App 一個js檔建議只有一個自訂元件
 function AppFunction(props) {
-  console.log(props)
+  //console.log(props)
 
   //呼叫useState勾子，設定初始化值為０，回傳一組getter和setter
   const [total, setTotal] = useState(props.initValue)
@@ -21,28 +21,3 @@ function AppFunction(props) {
 
 //輸出函式
 export default AppFunction
-
-//-------以下為原本的index畫面------------------------
-
-// import logo from './logo.svg';
-// import './App.css';
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
