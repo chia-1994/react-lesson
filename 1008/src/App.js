f0b0b43e9f3a918617568ee23a6658dbd6822b6a
Maureen <chiamaureenla@gmail.com>
2020-10-15 11:55:43 +0800

    加入react icon 跟 react bootstrap

--- 1008/src/App.js
+++ 1008/src/App.js
@@ -1,11 +1,34 @@
 // 導入其它的模組
 import React, { useState } from 'react'
-import Clock from './components/Clock'
+import MyNavbar from './components/MyNavbar'
+import MyFooter from './components/MyFooter'
+
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { AiFillHeart } from 'react-icons/ai'
 
 function App(props) {
   return (
     <>
-      <Clock />
+      <MyNavbar />
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <h1 className="mt-5">React Bootstrap</h1>
+            <Button variant="primary" className="mt-5">
+              Primary
+            </Button>
+            <Button variant="secondary" className="mt-5">
+              Secondary
+            </Button>
+            <Button variant="success" className="mt-5">
+              Success
+            </Button>
+          </Col>
+          <AiFillHeart className="mt-5" color="#6f42c1" size={150} />
+        </Row>
+      </Container>
+
+      <MyFooter />
     </>
   )
 }
