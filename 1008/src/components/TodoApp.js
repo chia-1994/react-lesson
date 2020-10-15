e6dc451b8f2e2a183dc80962098cee8a6b2c462d
Maureen <chiamaureenla@gmail.com>
2020-10-15 16:11:27 +0800

    待辦事項-刪除線線

--- 1008/src/components/TodoApp.js
+++ 1008/src/components/TodoApp.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react'
 
 function TodoApp(props) {
   const [todoInput, setTodoInput] = useState('')
-  const [todos, setTodos] = useState(['買豆漿', '買飯糰'])
+  //將每個待辦事項改為物件值 { id: 1, text: 'string', completed: bool }
+  const [todos, setTodos] = useState([
+    { id: 1, text: '買蛋餅', completed: false },
+    { id: 2, text: '買飯糰', completed: true },
+  ])
 
   return (
     <>
@@ -14,8 +18,15 @@ function TodoApp(props) {
         onKeyPress={(e) => {
           //按enter可輸入  但輸入''不會輸入
           if (e.key === 'Enter' && e.target.value !== '') {
+            // 建立一個新的todo項目
+            const newItem = {
+              id: +new Date(),
+              text: e.target.value,
+              completed: false,
+            }
+
             //...展開運算符 建立新的陣列（合併原本的todos陣列中的值）
-            const newTodos = [e.target.value, ...todos]
+            const newTodos = [newItem, ...todos]
 
             //設定todos狀態值
             setTodos(newTodos)
@@ -27,9 +38,17 @@ function TodoApp(props) {
       />
       <hr />
       <ul>
-        {todos.map((value, index) => {
-          return <li key={index}>{value}</li>
-        })}
+        {/* map使用時通常子元素會要求唯一key值(id值)  */}
+        {/* 這裡用索引值作為key值(id值)  */}
+        {todos.map((value, index) =>
+          value.completed ? (
+            <li key={value.text}>
+              <del>{value.text}</del>
+            </li>
+          ) : (
+            <li key={value.text}>{value.text}</li>
+          )
+        )}
       </ul>
     </>
   )
