02298a4d81f89a8e406349a21c6206ce438844b6
Maureen <chiamaureenla@gmail.com>
2020-10-16 14:36:30 +0800

    +編輯按鈕

--- 1008/src/components/TodoApp.js
+++ 1008/src/components/TodoApp.js
@@ -6,9 +6,47 @@ function TodoApp(props) {
   const [todoInput, setTodoInput] = useState('')
   //將每個待辦事項改為物件值 { id: 1, text: 'string', completed: bool }
   const [todos, setTodos] = useState([
-    { id: 1, text: '買蛋餅', completed: false },
-    { id: 2, text: '買飯糰', completed: true },
+    { id: 1, text: '買蛋餅', completed: false, edited: false },
+    { id: 2, text: '買飯糰', completed: true, edited: false },
   ])
+
+  // 利用id值尋找對應的item的索引值，然後改變edited值
+  const handleEditedToggle = (id) => {
+    // 先複製一個新的todos陣列
+    const newTodos = [...todos]
+    // 利用id值尋找對應的item的索引值
+    const todoItemIndex = newTodos.findIndex((item) => item.id === id)
+
+    // 如果尋找到的索引值不是-1時，代表有找到索引值
+    if (todoItemIndex !== -1) {
+      // 布林值 true變false，false變true
+      newTodos[todoItemIndex].edited = !newTodos[todoItemIndex].edited
+
+      // 設定回原本的todos
+      setTodos(newTodos)
+    }
+  }
+
+  // 利用id值尋找對應的item的索引值，然後改變text值
+  const handleEditedSave = (id, newText) => {
+    // 先複製一個新的todos陣列
+    const newTodos = [...todos]
+
+    // 利用id值尋找對應的item的索引值
+    const todoItemIndex = newTodos.findIndex((item) => item.id === id)
+
+    // 如果尋找到的索引值不是-1時，代表有找到索引值
+    if (todoItemIndex !== -1) {
+      // text 更改為新的 newText
+      newTodos[todoItemIndex].text = newText
+
+      // 設定回原本的todos
+      setTodos(newTodos)
+
+      // 切換回原本的狀態
+      handleEditedToggle(id)
+    }
+  }
 
   //利用id值尋找對應的item的索引值，然後改變completed值
   const handleCompleted = (id) => {
@@ -49,8 +87,10 @@ function TodoApp(props) {
       <hr />
       <TodoList
         todos={todos}
+        handleEditedToggle={handleEditedToggle}
         handleCompleted={handleCompleted}
         handleDelete={handleDelete}
+        handleEditedSave={handleEditedSave}
       />
     </>
   )
--- 1008/src/components/todo/TodoItem.js
+++ 1008/src/components/todo/TodoItem.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { AiFillDelete } from 'react-icons/ai'
+import { AiFillEdit } from 'react-icons/ai'
 
 function TodoItem(props) {
   // 解構賦值的語法，先把要用的變數值從props解出來
-  const { text, completed, completedMethod, deleteMethod } = props
+  const {
+    text,
+    completed,
+    completedMethod,
+    deleteMethod,
+    editedToggleMethod,
+  } = props
   return (
     <>
       <li>
         <input type="checkbox" checked={completed} onChange={completedMethod} />
         {/* 用completed來判斷是否要有刪除線，true則要有 */}
         {completed ? <del>{text}</del> : text}
-        <button onClick={deleteMethod}>刪除</button>
+        <span onClick={editedToggleMethod}>
+          <AiFillEdit />
+        </span>
+        <span onClick={deleteMethod}>
+          <AiFillDelete />
+        </span>
       </li>
     </>
   )
