ca95f830f2abd8edc615e0614e75e9af9b695840
Maureen <chiamaureenla@gmail.com>
2020-10-21 14:07:20 +0800

    載入thunk

--- 1008/src/index.js
+++ 1008/src/index.js
@@ -6,34 +6,46 @@ import * as serviceWorker from './serviceWorker'
 
 //以下為redux
 //第一步：匯入createStore, combineReducers API
-import { createStore, combineReducers } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+
+// 導入redux-thunk中介軟體
+import thunk from 'redux-thunk'
 
 // 匯入綁定react與redux用的最上層元件
 import { Provider } from 'react-redux'
 
+// 第二步：寫出reducer
+import { rootReducer } from './reducers'
 //第二步：寫出reducer：純粹函式---給狀態、動作，回傳新的狀態
 //定義action
 // action = {type: 'ADD_VALUE', value: 1 }
 //action = {type: 'MINUS_VALUE', value: 1 }
-function counter(state = 0, action) {
-  switch (action.type) {
-    case 'ADD_VALUE':
-      return state + action.value
-    case 'MINUS_VALUE':
-      return state - action.value
-    default:
-      return state
-  }
-}
+// function counter(state = 0, action) {
+//   switch (action.type) {
+//     case 'ADD_VALUE':
+//       return state + action.value
+//     case 'MINUS_VALUE':
+//       return state - action.value
+//     default:
+//       return state
+//   }
+// }
 // 第二之一：合併所有的reducers成一個大的reducer
-const rootReducer = combineReducers({
-  counter,
-})
+// const rootReducer = combineReducers({
+//   counter,
+// })
 
 // 第三步：由rootReducer建立store
+// const store = createStore(
+//   rootReducer /* preloadedState, */,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+
+// 在建立store時要加入中介軟體
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
-  rootReducer /* preloadedState, */,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  rootReducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
 )
 
 ReactDOM.render(
