a867ba1e51a01fddbf88e93d8c38b7b1db4e468e
Maureen <chiamaureenla@gmail.com>
2020-11-16 11:29:36 +0800

    final

--- 1008/src/components/CommentApp.js
+++ 1008/src/components/CommentApp.js
@@ -178,11 +178,42 @@ function CommentApp(props) {
     }, 500)
   }
 
-  const handleDelete = (sid) => {
-    //建立一個新的陣列，其中"不包含"要被移除的項目(用filter)
-    const newComment = comment.filter((item, index) => item.sid !== sid)
+  // const handleDelete = (sid) => {
+  //   //建立一個新的陣列，其中"不包含"要被移除的項目(用filter)
+  //   const newComment = comment.filter((item, index) => item.sid !== sid)
 
-    setComment(newComment)
+  //   setComment(newComment)
+  // }
+
+  async function deleteCommentFromServer(sid) {
+    // 開啟載入指示
+    setDataLoading(true)
+
+    // 連接的伺服器資料網址
+    const url = 'http://localhost:3000/comment/del/' + sid
+
+    // 注意header資料格式要設定，伺服器才知道是json格式
+    const request = new Request(url, {
+      method: 'DELETE',
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'appliaction/json',
+      }),
+    })
+
+    const response = await fetch(request)
+    const data = await response.json()
+    console.log(data)
+
+    // 設定資料
+    if (!comment.sid) {
+      const newComment = comment.filter((item, index) => {
+        return item.sid !== sid
+      })
+
+      setComment(newComment)
+      alert('已刪除')
+    }
   }
 
   return (
@@ -232,7 +263,9 @@ function CommentApp(props) {
                       <p key={item.id}>{item.review}</p>
                       <div className="comment-time"></div>
                     </div>
-                    <button onClick={() => handleDelete(item.sid)}>刪除</button>
+                    <button onClick={() => deleteCommentFromServer(item.sid)}>
+                      刪除
+                    </button>
                   </Col>
                 </>
               )
